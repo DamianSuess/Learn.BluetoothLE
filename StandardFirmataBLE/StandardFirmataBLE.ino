96c8390eae749b18f5b3525845f71dd980b28e10
Damian Suess <djsuess@gmail.com>
2022-02-24 20:07:19 -0500

    Firmata update

--- StandardFirmataBLE/StandardFirmataBLE.ino
+++ StandardFirmataBLE/StandardFirmataBLE.ino
@@ -2,18 +2,24 @@
   Firmata is a generic protocol for communicating with microcontrollers
   from software on a host computer. It is intended to work with
   any host computer software package.
+
   To download a host software package, please click on the following link
   to open the list of Firmata client libraries in your default browser.
+
   https://github.com/firmata/arduino#firmata-client-libraries
+
   Copyright (C) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
   Copyright (C) 2010-2011 Paul Stoffregen.  All rights reserved.
   Copyright (C) 2009 Shigeru Kobayashi.  All rights reserved.
   Copyright (C) 2009-2016 Jeff Hoefs.  All rights reserved.
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
+
   See file LICENSE.txt for further informations on licensing terms.
+
   Last updated October 16th, 2016
 */
 
@@ -784,7 +790,6 @@ void setup()
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
   
   Bluefruit.begin();
-  Bluefruit.setName("Bluefruit52");
   Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
   
   // try to go as fast as possible, could be rejected by some central, increase it if needed
--- StandardFirmataBLE/readme.md
+++ StandardFirmataBLE/readme.md
@@ -3,3 +3,4 @@
 ## References
 
 * [BLE Pin I-O - Bluefruit nRF52 Feather Learning Guide - Adafruit Learning System](https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/ble-pin-i-o)
+* [Source](https://github.com/adafruit/Adafruit_nRF52_Arduino/blob/master/libraries/Bluefruit52Lib/examples/Peripheral/StandardFirmataBLE/StandardFirmataBLE.ino)
