6915be1b4a30d65343ed63579e487c76c460bb59
Damian Suess <djsuess@gmail.com>
2022-05-26 15:52:20 -0400

    The Prism.Maui NuGet is not ready yet for production use yet. It's missing some hookups

--- CrossMaui.ShinyBle/CrossMaui.ShinyBle/MauiProgram.cs
+++ CrossMaui.ShinyBle/CrossMaui.ShinyBle/MauiProgram.cs
@@ -1,19 +1,51 @@
-﻿namespace CrossMaui.ShinyBle;
+﻿using Prism;
+using Prism.DryIoc.Maui;
 
+namespace CrossMaui.ShinyBle;
+
+// References:
+//  - https://github.com/dansiegel/Prism.Maui
+//  - https://www.nuget.org/packages/Prism.Maui/
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder();
-		builder
+    return MauiApp.CreateBuilder()
+      .UsePrismApp<App>()
+      .RegisterTypes(containerRegistry =>
+      {
+        containerRegistry.RegisterGlobalNavigationObserver();
+        containerRegistry.RegisterForNavigation<MainPage>();
+        containerRegistry.RegisterForNavigation<RootPage>();
+        containerRegistry.RegisterForNavigation<SamplePage>();
+      })
+      .OnAppStart
+      ////.OnAppStart(navigationService => navigationService.CreateBuilder()
+      ////  .AddNavigationSegment("MainPage")
+      ////  .AddNavigationPage()
+      ////  .AddNavigationSegment<ViewAViewModel>()
+      ////  .AddNavigationSegment("ViewB")
+      ////  .Navigate(HandleNavigationError))
+      .ConfigureFonts(fonts =>
+      {
+        fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+      })
+      .Build();
 
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    ////var builder = MauiApp.CreateBuilder()
+    ////  .UseMauiApp<App>()
+    ////	.ConfigureFonts(fonts =>
+    ////	{
+    ////		fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+    ////		fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+    ////	});
+    ////
+    ////return builder.Build();
+  }
 
-		return builder.Build();
-	}
+  private static void HandleNavigationError(Exception ex)
+  {
+    Console.WriteLine(ex);
+    System.Diagnostics.Debugger.Break();
+  }
 }
--- CrossMaui.ShinyBle/CrossMaui.ShinyBle/Platforms/Android/MainActivity.cs
+++ CrossMaui.ShinyBle/CrossMaui.ShinyBle/Platforms/Android/MainActivity.cs
@@ -4,7 +4,22 @@ using Android.OS;
 
 namespace CrossMaui.ShinyBle;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+  Theme = "@style/Maui.SplashTheme",
+  MainLauncher = true,
+  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+  protected override void OnCreate(Bundle savedInstanceState)
+  {
+    base.OnCreate(savedInstanceState);
+    Platform.Init(this, savedInstanceState);
+  }
+
+  public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+  {
+    Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+  }
 }
